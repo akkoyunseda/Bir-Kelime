5172050361473894f54af8d2753f40e166d4e417
Sedanur Akkoyun <akkoyunsedanur@gmail.com>
2022-07-10 17:15:57 +0300

    C# ile Bir Kelime Oyunu
    C# ile Bir Kelime Oyunu Windows Form Uygulaması

---
+++ BirKelime/BirKelime/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace BirKelime
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnharf1 = new System.Windows.Forms.Button();
+            this.btnharf2 = new System.Windows.Forms.Button();
+            this.btnharf3 = new System.Windows.Forms.Button();
+            this.btnharf4 = new System.Windows.Forms.Button();
+            this.btnharf5 = new System.Windows.Forms.Button();
+            this.btnharf6 = new System.Windows.Forms.Button();
+            this.btnharf7 = new System.Windows.Forms.Button();
+            this.btnharf8 = new System.Windows.Forms.Button();
+            this.btnjoker = new System.Windows.Forms.Button();
+            this.btngetir = new System.Windows.Forms.Button();
+            this.lblpuan = new System.Windows.Forms.Label();
+            this.txtPuan = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.kelimelist = new System.Windows.Forms.ListBox();
+            this.btnara = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnharf1
+            // 
+            this.btnharf1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnharf1.Location = new System.Drawing.Point(44, 99);
+            this.btnharf1.Name = "btnharf1";
+            this.btnharf1.Size = new System.Drawing.Size(60, 56);
+            this.btnharf1.TabIndex = 0;
+            this.btnharf1.UseVisualStyleBackColor = true;
+            // 
+            // btnharf2
+            // 
+            this.btnharf2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnharf2.Location = new System.Drawing.Point(110, 99);
+            this.btnharf2.Name = "btnharf2";
+            this.btnharf2.Size = new System.Drawing.Size(60, 56);
+            this.btnharf2.TabIndex = 1;
+            this.btnharf2.UseVisualStyleBackColor = true;
+            // 
+            // btnharf3
+            // 
+            this.btnharf3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnharf3.Location = new System.Drawing.Point(176, 99);
+            this.btnharf3.Name = "btnharf3";
+            this.btnharf3.Size = new System.Drawing.Size(60, 56);
+            this.btnharf3.TabIndex = 2;
+            this.btnharf3.UseVisualStyleBackColor = true;
+            // 
+            // btnharf4
+            // 
+            this.btnharf4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnharf4.Location = new System.Drawing.Point(242, 99);
+            this.btnharf4.Name = "btnharf4";
+            this.btnharf4.Size = new System.Drawing.Size(60, 56);
+            this.btnharf4.TabIndex = 3;
+            this.btnharf4.UseVisualStyleBackColor = true;
+            // 
+            // btnharf5
+            // 
+            this.btnharf5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnharf5.Location = new System.Drawing.Point(308, 99);
+            this.btnharf5.Name = "btnharf5";
+            this.btnharf5.Size = new System.Drawing.Size(60, 56);
+            this.btnharf5.TabIndex = 4;
+            this.btnharf5.UseVisualStyleBackColor = true;
+            // 
+            // btnharf6
+            // 
+            this.btnharf6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnharf6.Location = new System.Drawing.Point(374, 99);
+            this.btnharf6.Name = "btnharf6";
+            this.btnharf6.Size = new System.Drawing.Size(60, 56);
+            this.btnharf6.TabIndex = 5;
+            this.btnharf6.UseVisualStyleBackColor = true;
+            // 
+            // btnharf7
+            // 
+            this.btnharf7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnharf7.Location = new System.Drawing.Point(440, 99);
+            this.btnharf7.Name = "btnharf7";
+            this.btnharf7.Size = new System.Drawing.Size(60, 56);
+            this.btnharf7.TabIndex = 6;
+            this.btnharf7.UseVisualStyleBackColor = true;
+            // 
+            // btnharf8
+            // 
+            this.btnharf8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnharf8.Location = new System.Drawing.Point(506, 99);
+            this.btnharf8.Name = "btnharf8";
+            this.btnharf8.Size = new System.Drawing.Size(60, 56);
+            this.btnharf8.TabIndex = 7;
+            this.btnharf8.UseVisualStyleBackColor = true;
+            // 
+            // btnjoker
+            // 
+            this.btnjoker.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnjoker.Location = new System.Drawing.Point(656, 149);
+            this.btnjoker.Name = "btnjoker";
+            this.btnjoker.Size = new System.Drawing.Size(76, 56);
+            this.btnjoker.TabIndex = 8;
+            this.btnjoker.UseVisualStyleBackColor = true;
+            // 
+            // btngetir
+            // 
+            this.btngetir.BackColor = System.Drawing.Color.Transparent;
+            this.btngetir.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btngetir.Location = new System.Drawing.Point(44, 32);
+            this.btngetir.Name = "btngetir";
+            this.btngetir.Size = new System.Drawing.Size(230, 43);
+            this.btngetir.TabIndex = 9;
+            this.btngetir.Text = "Harfleri Getir";
+            this.btngetir.UseVisualStyleBackColor = false;
+            this.btngetir.Click += new System.EventHandler(this.btngetir_Click);
+            // 
+            // lblpuan
+            // 
+            this.lblpuan.AutoSize = true;
+            this.lblpuan.BackColor = System.Drawing.Color.Transparent;
+            this.lblpuan.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblpuan.Location = new System.Drawing.Point(664, 267);
+            this.lblpuan.Name = "lblpuan";
+            this.lblpuan.Size = new System.Drawing.Size(68, 29);
+            this.lblpuan.TabIndex = 11;
+            this.lblpuan.Text = "Puan";
+            // 
+            // txtPuan
+            // 
+            this.txtPuan.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtPuan.Location = new System.Drawing.Point(656, 314);
+            this.txtPuan.Name = "txtPuan";
+            this.txtPuan.Size = new System.Drawing.Size(94, 36);
+            this.txtPuan.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(41, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 24);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "1.Harf";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(111, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 24);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "2.Harf";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(177, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 24);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "3.Harf";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(243, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 24);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "4.Harf";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(309, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 24);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "5.Harf";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(375, 169);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 24);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "6.Harf";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(441, 169);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 24);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "7.Harf";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(507, 169);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 24);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "8.Harf";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(638, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 38);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Joker Harf";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // kelimelist
+            // 
+            this.kelimelist.FormattingEnabled = true;
+            this.kelimelist.ItemHeight = 16;
+            this.kelimelist.Location = new System.Drawing.Point(44, 314);
+            this.kelimelist.Name = "kelimelist";
+            this.kelimelist.Size = new System.Drawing.Size(246, 132);
+            this.kelimelist.TabIndex = 23;
+            // 
+            // btnara
+            // 
+            this.btnara.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnara.Location = new System.Drawing.Point(44, 249);
+            this.btnara.Name = "btnara";
+            this.btnara.Size = new System.Drawing.Size(192, 47);
+            this.btnara.TabIndex = 24;
+            this.btnara.Text = "Kelime Ara";
+            this.btnara.UseVisualStyleBackColor = true;
+            this.btnara.Click += new System.EventHandler(this.btnara_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.BackgroundImage = global::BirKelime.Properties.Resources.MindTheGap_Collection1_VintageLetters_WP20018_01;
+            this.ClientSize = new System.Drawing.Size(872, 586);
+            this.Controls.Add(this.btnara);
+            this.Controls.Add(this.kelimelist);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPuan);
+            this.Controls.Add(this.lblpuan);
+            this.Controls.Add(this.btngetir);
+            this.Controls.Add(this.btnjoker);
+            this.Controls.Add(this.btnharf8);
+            this.Controls.Add(this.btnharf7);
+            this.Controls.Add(this.btnharf6);
+            this.Controls.Add(this.btnharf5);
+            this.Controls.Add(this.btnharf4);
+            this.Controls.Add(this.btnharf3);
+            this.Controls.Add(this.btnharf2);
+            this.Controls.Add(this.btnharf1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnharf1;
+        private System.Windows.Forms.Button btnharf2;
+        private System.Windows.Forms.Button btnharf3;
+        private System.Windows.Forms.Button btnharf4;
+        private System.Windows.Forms.Button btnharf5;
+        private System.Windows.Forms.Button btnharf6;
+        private System.Windows.Forms.Button btnharf7;
+        private System.Windows.Forms.Button btnharf8;
+        private System.Windows.Forms.Button btnjoker;
+        private System.Windows.Forms.Button btngetir;
+        private System.Windows.Forms.Label lblpuan;
+        private System.Windows.Forms.TextBox txtPuan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox kelimelist;
+        private System.Windows.Forms.Button btnara;
+    }
+}
+
---
+++ BirKelime/BirKelime/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BirKelime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        string[] harfler = new string[8];
+
+        private void btngetir_Click(object sender, EventArgs e)
+        {
+            string[] dizi = { "A", "B", "C", "Ç", "D", "E", "F", "G", "Ğ", "H", "I", "İ", "J", "K", "L", "M", "N", "O", "Ö", "P", "R", "S", "Ş", "T", "U", "Ü", "V", "Y", "Z" };
+            
+
+           btnharf1.Text = dizi[rnd.Next(0, dizi.Length)];
+           btnharf2.Text = dizi[rnd.Next(0, dizi.Length)];
+           btnharf3.Text = dizi[rnd.Next(0, dizi.Length)];
+           btnharf4.Text = dizi[rnd.Next(0, dizi.Length)];
+           btnharf5.Text = dizi[rnd.Next(0, dizi.Length)];
+           btnharf6.Text = dizi[rnd.Next(0, dizi.Length)];
+           btnharf7.Text = dizi[rnd.Next(0, dizi.Length)];
+           btnharf8.Text = dizi[rnd.Next(0, dizi.Length)];
+
+            harfler[0] = btnharf1.Text;
+            harfler[1] = btnharf2.Text;
+            harfler[2] = btnharf3.Text;
+            harfler[3] = btnharf4.Text;
+            harfler[4] = btnharf5.Text;
+            harfler[5] = btnharf6.Text;
+            harfler[6] = btnharf7.Text;
+            harfler[7] = btnharf8.Text;
+           
+
+
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] dizi = { "A", "B", "C", "D", "E", "F", "G", "Ğ", "H", "I", "İ", "J", "K", "L", "M", "N", "O", "Ö", "P", "R", "S", "Ş", "T", "U", "Ü", "V", "Y", "Z" };
+
+            btnjoker.Text = dizi[rnd.Next(0, dizi.Length)];
+
+           
+        }
+
+        private void btnara_Click(object sender, EventArgs e)
+        {
+            string dosya_yolu = @"C:\Users\akkoy\Desktop\kelimeler.txt";
+            string[] words = System.IO.File.ReadAllLines(dosya_yolu, Encoding.GetEncoding("windows-1254"));
+
+           
+        }
+    }
+}
